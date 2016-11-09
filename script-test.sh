a935ec6ce1012d2a78c1bd102a64ffa970ec385a
Jamie Ramos <jmramos@live.unc.edu>
2016-11-09 21:35:49 +0000

    updated script

--- script-test.sh
+++ script-test.sh
@@ -1,28 +1,48 @@
 #!/bin/bash
 
+#add timestamp to desired file
 timestamp=$(date +"%F_%T")
 
-echo $timestamp > ./example.list
+#find out desired filename
+echo "In which csv file would you like to save these results? Do not include .csv"
+read FILENAME
 
+#choose whether to overwrite or append
+echo "Would you like to overwrite or append to that file? [o/a]"
+read CHOICE
+
+#add timestamp
+echo $timestamp >./$FILENAME.list
+
+#generate randomID
 UniqueID=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 16 | head -n 1)
 
-echo $UniqueID >> ./example.list
+#echo uniqueID to file
+echo $UniqueID >> ./$FILENAME.list
 
+#bold questions
 bold=$(tput bold)
 
-
-
+#ask questions
 read -p "${bold}What is your favorite color? "  question1_ans
-read -p "${bold}Do you prefer mexican food over italian food? " question2_ans 
+read -p "${bold}Do you prefer Mexican food over Italian food? " question2_ans 
 read -p "${bold}What's your remedy for a sweet tooth? " question3_ans
 read -p "${bold}What do you want to do when you grow up? " question4_ans 
 read -p "${bold}Where is your dream vacation location? " question5_ans
 
+#pipe questions
+echo $question1_ans >> ./$FILENAME.list
+echo $question2_ans >> ./$FILENAME.list
+echo $question3_ans >> ./$FILENAME.list
+echo $question4_ans >> ./$FILENAME.list
+echo $question5_ans >> ./$FILENAME.list
 
-echo $question1_ans >> ./example.list
-echo $question2_ans >> ./example.list
-echo $question3_ans >> ./example.list
-echo $question4_ans >> ./example.list
-echo $question5_ans >> ./example.list
+#append or overwrite desired csv file
+#overwrite
+if [[ "$CHOICE" == "o" ]]; then paste -d, -s ./$FILENAME.list > ./$FILENAME.csv
+echo "The responses have been recorded. File $FILENAME.csv was overwritten." 
+#append
+elif [[ "$CHOICE" == "a" ]]; then paste -d, -s ./$FILENAME.list >> ./$FILENAME.csv
+echo "The responses have been recorded. File $FILENAME.csv was appended."
 
-paste -d, -s ./example.list >> ./example.csv
+fi
