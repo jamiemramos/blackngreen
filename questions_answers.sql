5f9467879a7966f6e14db416a62491057bc6f6ac
Gavriella <gavvy@live.unc.edu>
2016-11-16 21:03:33 +0000

    new answers

--- questions_answers.sql
+++ questions_answers.sql
@@ -39,7 +39,7 @@ CREATE TABLE `questions_answers` (
 
 LOCK TABLES `questions_answers` WRITE;
 /*!40000 ALTER TABLE `questions_answers` DISABLE KEYS */;
-INSERT INTO `questions_answers` VALUES ('2016-11-02_19:55:30','SkFyg9trItp03Jzm','green','depends on my mood','realllly luxurious desserts or gourmet chocolate','start an empire',' travel'),('2016-11-02_19:57:51','OdqZ3z8UN9fcmY5O','spring leaves','neither','milkshake','change things around and be happy','somewhere tropical'),('2016-11-02_20:01:10','rXjJGZCHaj2grRa8','green','Mexican food','Reeses','Librarian','Italy'),('2016-11-02_20:01:34','joVyUeBbRN1DaE1b','Red','Italian','Milky Way','Musician','Morocco'),('2016-11-16_14:40:21','G1d8c6HLnV7kIwUT','Yellow','Italian','Peppermint','Dance','Idaho'),('2016-11-16_14:42:58','gWd1ils2iuTDfthf','Purple','Mexican','Almond Joy','Sing','Canada'),('2016-11-16_20:51:01','AmImWGjhyjj5VeH4','Red','Italian','Cheesecake','Astronaut','Sweden'),('2016-11-16_20:53:53','6e3QSOl3kxiXzlVh','Aquamarine','Italian','Pie','President','Fiji');
+INSERT INTO `questions_answers` VALUES ('2016-11-02_19:55:30','SkFyg9trItp03Jzm','green','depends on my mood','realllly luxurious desserts or gourmet chocolate','start an empire',' travel'),('2016-11-02_19:57:51','OdqZ3z8UN9fcmY5O','spring leaves','neither','milkshake','change things around and be happy','somewhere tropical'),('2016-11-02_20:01:10','rXjJGZCHaj2grRa8','green','Mexican food','Reeses','Librarian','Italy'),('2016-11-02_20:01:34','joVyUeBbRN1DaE1b','Red','Italian','Milky Way','Musician','Morocco'),('2016-11-16_14:40:21','G1d8c6HLnV7kIwUT','Yellow','Italian','Peppermint','Dance','Idaho'),('2016-11-16_14:42:58','gWd1ils2iuTDfthf','Purple','Mexican','Almond Joy','Sing','Canada'),('2016-11-16_20:51:01','AmImWGjhyjj5VeH4','Red','Italian','Cheesecake','Astronaut','Sweden'),('2016-11-16_20:53:53','6e3QSOl3kxiXzlVh','Aquamarine','Italian','Pie','President','Fiji'),('2016-11-16_20:58:29','EDXWPVfORl9eVunX','liven oak','gourmet cheese','stealing my roommates ice cream','inspire people','anywhere but here'),('2016-11-16_21:01:14','2EGcz0zwESLWdgqe','all shades of purple rn','mexi sounds good today','self deprecation','everything','in the sky');
 /*!40000 ALTER TABLE `questions_answers` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -52,4 +52,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2016-11-16 20:54:28
+-- Dump completed on 2016-11-16 21:02:20
